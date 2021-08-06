75eed335da6690352f530fcda580244d08e84023
MarkGhebrial <mghebrialjr@icloud.com>
2021-08-05 19:59:57 -0700

    ClimberSubsystem boilerplate

---
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimberSubsystem extends SubsystemBase {
+    public ClimberSubsystem () {
+
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+}
