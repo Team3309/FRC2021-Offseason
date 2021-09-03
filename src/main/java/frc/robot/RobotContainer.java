80513dc9569ce6d05c49826d49175095c661e441
MarkGhebrial <mghebrialjr@icloud.com>
2021-09-02 17:30:53 -0700

    Create LambdaTrigger class

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@ import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.SerializerSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
+import friarLib2.hid.LambdaTrigger;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -58,7 +59,7 @@ public class RobotContainer {
     .whenPressed(() -> climber.deploy(), climber);
 
     /** When the left trigger is pressed, activate the winch motor */
-    new JoystickButton(OperatorInterface.OperatorController, XboxController.Axis.kLeftTrigger.value)
+    new LambdaTrigger(() -> {return true;})
     .whileActiveOnce(new Climb(climber));
   }
 
