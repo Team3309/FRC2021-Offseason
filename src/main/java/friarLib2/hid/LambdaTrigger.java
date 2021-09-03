5ad6d8aec392fb88eeecee98444b91837e78c639
Mark Ghebrial <mghebrialjr@icloud.com>
2021-09-02 18:23:27 -0700

    Merge branch 'lambda-triggers' into 'climber-bindings'

---
+++ src/main/java/friarLib2/hid/LambdaTrigger.java
@@ -0,0 +1,22 @@
+package friarLib2.hid;
+
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+public class LambdaTrigger extends Trigger {
+
+    TriggerCondition condition;
+
+    public LambdaTrigger (TriggerCondition condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public boolean get () {
+        System.out.println(condition.get());
+        return condition.get();
+    }
+
+    public interface TriggerCondition {
+        boolean get();
+    }
+}
