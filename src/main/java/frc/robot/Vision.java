9eab449712af52dcdbbf6d74fd21eb299ac3186e
MarkGhebrial <mghebrialjr@icloud.com>
2021-09-11 15:17:04 -0700

    Calculate distance from target using trig

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -1,7 +1,11 @@
 package frc.robot;
 
+import org.photonvision.PhotonCamera;
+
+import frc.robot.subsystems.ArmSubsystem;
 import friarLib2.vision.PhotonCameraWrapper;
 import friarLib2.vision.VisionCamera;
+import friarLib2.vision.utility.PixelToAngle;
 
 /**
  * Container for the vision systems
@@ -12,7 +16,22 @@ public class Vision {
     /**
      * @return the distance in meters from the target
      */
-    public static double getDistanceFromTarget () {
-        return mainCamera.getBestTarget().getPose().getX();
+    public static double getDistanceFromTarget (ArmSubsystem arm) {
+        if (mainCamera instanceof PhotonCamera) {
+            return mainCamera.getBestTarget().getPose().getX();
+        } else {
+            // Constants for Limelight
+            PixelToAngle p = new PixelToAngle(320, 240, 54, 41);
+
+            // Magic equations are based on the physical dimensions of the robot
+            double heightOfCamera = 14.93 + (21.297 * Math.toRadians(Math.sin(32.90)));
+            double angleOfCamera = (90 - 15.47) - arm.getArmPosition();
+
+            // From dimensions of field
+            final double HEIGHT_OF_TARGET = 97.5 - (30.0 / 4.0);
+
+            // See https://docs.limelightvision.io/en/latest/cs_estimating_distance.html
+            return (HEIGHT_OF_TARGET - heightOfCamera) / Math.tan(Math.toRadians(angleOfCamera) + Math.toRadians(p.calculateYAngle(mainCamera.getBestTarget())));
+        }
     }
 }
--- src/main/java/friarLib2/vision/utility/PixelToAngle.java
+++ src/main/java/friarLib2/vision/utility/PixelToAngle.java
@@ -14,7 +14,7 @@ public class PixelToAngle {
      * <p>Y is up/down and X is right/left
      * 
      * <p>See https://docs.limelightvision.io/en/latest/theory.html#from-pixels-to-angles
-     * 
+     * for theory and equations
      */
     public PixelToAngle (double resolutionX, double resolutionY, double horizontalFOV, double verticalFOV) {
         this.resolutionX = resolutionX;
@@ -27,7 +27,7 @@ public class PixelToAngle {
         viewplaneHeight = 2.0 * Math.tan(Math.toRadians(verticalFOV) / 2);
     }
 
-    public double calculateXAngle (double cameraAngle, double cameraXOffset, VisionTarget target) {
+    public double calculateXAngle (VisionTarget target) {
         // Normalize pixel coordinates so that they range from -1 to 1, with (0, 0) being the center of the image
         double normalizedX = (target.getX() - (resolutionX / 2.0)) / resolutionX;
 
@@ -38,7 +38,7 @@ public class PixelToAngle {
         return Math.atan2(1, x);
     }
 
-    public double calculateYAngle (double cameraAngle, double cameraXOffset, VisionTarget target) {
+    public double calculateYAngle (VisionTarget target) {
         // Normalize pixel coordinates so that they range from -1 to 1, with (0, 0) being the center of the image
         double normalizedY = ((resolutionY / 2.0) - target.getY()) / resolutionY;
 
